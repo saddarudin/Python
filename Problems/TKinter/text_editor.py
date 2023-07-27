35b47d8f8acf8c0ea2e4724f00d4658e9fdbfe24
Saddar U Din Babar <108076528+saddarudin@users.noreply.github.com>
2023-07-27 16:55:52 +0500

    Added save_as and open_file methods

--- Problems/TKinter/text_editor.py
+++ Problems/TKinter/text_editor.py
@@ -1,19 +1,52 @@
 from tkinter import *
+from tkinter import filedialog
 
 
-class ClickHere:
+class WriteNote:
+
+    def open_file(self):
+        x = filedialog.askopenfile(initialdir="/", title="open file", filetypes=(("text files", "*.txt"),("all files", "*.*")))
+        if x is not None:
+            self.text_area.delete(1.0, END)
+            for line in x:
+                self.text_area.insert(END, line)
+        x.close()
+
+    def save_as_file(self):
+        f = filedialog.asksaveasfile(mode="w", defaultextension=".txt")
+        if f is None:
+            return
+        f.write(self.text_area.get(1.0, END))
+        f.close()
+
     def __init__(self, master):
         self.master = master
-        self.master.title("text editor")
-        self.text = Text()
-        self.text.pack(fill=BOTH, expand=1)
-        self.main_menu = Menu(self.master)
-        self.file_menu = Menu(self.main_menu)
-        self.main_menu.add_cascade(label="File", menu=self.file_menu)
-        self.edit_menu = Menu(self.main_menu)
-        self.main_menu.add_cascade(label="Edit", menu=self.edit_menu)
+        master.title("Text Editor")
+        self.text_area = Text()
+        self.text_area.pack(fill=BOTH, expand=1)
+        self.main_menu = Menu()
+        self.master.config(menu=self.main_menu)
+        self.file = Menu()
+        self.main_menu.add_cascade(label="File", menu=self.file)
+        self.edit = Menu()
+        self.main_menu.add_cascade(label="edit", menu=self.edit)
+        self.view = Menu()
+        self.main_menu.add_cascade(label="view", menu=self.view)
+        self.file.add_command(label="open", command=self.open_file)
+        self.file.add_command(label="save", command="")
+        self.file.add_command(label="save as", command="")
+        self.edit.add_command(label="rename", command="")
+        self.edit.add_command(label="delete", command="")
+        self.edit.add_command(label="change extension", command="")
+        self.view.add_command(label="horizontal", command="")
+        self.view.add_command(label="vertical", command="")
+        self.view.add_command(label="full screen", command="")
+        self.view.add_command(label="half screen", command="")
+
 
 
 root = Tk()
-te = ClickHere(root)
+notepad = WriteNote(root)
+root.geometry("300x300+50+50")
 root.mainloop()
+
