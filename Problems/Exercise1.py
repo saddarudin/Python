441b498ab748f249069ac5544e316ffafc4d2fb2
Saddar U Din Babar <108076528+saddarudin@users.noreply.github.com>
2023-08-29 21:06:55 +0500

    Update Exercise1.py

--- Problems/Exercise1.py
+++ Problems/Exercise1.py
@@ -3,7 +3,7 @@
 # 2. these are mountains /\/\/\/\
 # 3. he is   awesome (use escape sequence instead of manual spaces)
 # 4. \" \n \t \'
-print("this is \\\\ double backslash")
-print("these are mountains /\\/\\/\\/\\")
-print('he is \tawesome')
+print("This is \\\\ double backslash")
+print("These are mountains /\\/\\/\\/\\")
+print('He is \tawesome')
 print("\\\" \\\n \\\t \\\'")
