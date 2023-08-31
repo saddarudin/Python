68ad72fb3ab43e4a622a3405e80bca5ae8dab169
Saddar U Din Babar <108076528+saddarudin@users.noreply.github.com>
2023-08-31 22:59:12 +0500

    Update stringFormatting.py

--- Problems/stringFormatting.py
+++ Problems/stringFormatting.py
@@ -2,5 +2,5 @@
 # and you have to print the average of
 # three numbers using string formatting
 
-a,b,c=input("Enter num1, num2, num3 (separated by comma)").split(",")
-print(f"The average of three numbers is {(int(a)+int(b)+int(c))/3}")
+a,b,c=int(input("Enter num1, num2, num3 (separated by comma)").split(","))
+print(f"The average of three numbers is {(a+b+c)/3}")
