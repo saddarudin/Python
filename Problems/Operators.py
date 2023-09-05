07505feb910c60416ebed7cf8cd7f9552203a42a
Saddar U Din Babar <108076528+saddarudin@users.noreply.github.com>
2023-09-05 22:25:41 +0500

    Update Operators.py

--- Problems/Operators.py
+++ Problems/Operators.py
@@ -1,17 +1,10 @@
-print(2+3)
-# 5
-print(4-1)
-# 3
-print(2*2)
-# 4
-print(4/3)
-# 1.33
-print(4//3)
-# 1
+print(2+3) # 5
+print(4-1) # 3
+print(2*2) # 4
+print(4/3) # 1.33
+print(4//3) # 1
 print(4.2/2)
 print(4.2//2)
 print(4%3)
-print(3**2)
-# 3^2=9
-# print square root of 4
-print(4**0.5)
+print(3**2) # 3^2=9
+print(4**0.5) # print square root of 4
