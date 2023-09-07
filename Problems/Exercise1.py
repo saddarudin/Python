31c582f9da0472b53499daf8f8d04447804e582a
Saddar U Din Babar <108076528+saddarudin@users.noreply.github.com>
2023-09-07 19:27:05 +0500

    Update Exercise1.py

--- Problems/Exercise1.py
+++ Problems/Exercise1.py
@@ -1,7 +1,7 @@
 # Print the following lines
-# 1. this is \\ double backslash
-# 2. these are mountains /\/\/\/\
-# 3. he is   awesome (use escape sequence instead of manual spaces)
+# 1. This is \\ double backslash
+# 2. These are mountains /\/\/\/\
+# 3. He is   awesome (use escape sequence instead of manual spaces)
 # 4. \" \n \t \'
 print("This is \\\\ double backslash")
 print("These are mountains /\\/\\/\\/\\")
